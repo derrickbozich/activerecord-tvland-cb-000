261b0c6fc20705fdddf4d7afb890a3f9cfcdd681
derrick b <dbozich1@gmail.com>
2018-06-26 06:43:34 +0000

    automatically backed up by learn

--- db/migrate/007_add_columns_to_shows.rb
+++ db/migrate/007_add_columns_to_shows.rb
@@ -1,5 +1,5 @@
 class AddColumnsToShows < ActiveRecord::Migration[4.2]
   def change
-    add_column :characters, :catchphrase, :string
+    add_column :shows, :day, :string, :season, :string, :genre, :string
   end
 end
